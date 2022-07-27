314c01e10bea9d07698c820c455be23fca9fb43f
manishkumarmandane <bm0369@gmail.com>
2022-07-27 22:21:53 +0100

    decnumber

---
+++ OddEvenumber.java
@@ -0,0 +1,22 @@
+package oddeven;
+
+public class OddEvenumber
+{
+    public static void main(String[] args)
+    {
+        int i ;
+        for(i=1; i<=10;i++)
+        {
+            if (i % 2 == 0)
+            {
+                //System.out.println("print even number");
+                System.out.print(+ i + "= Is Even"  );
+            }
+            else
+            {
+                System.out.print("\n" + i + " Is Odd   "   );
+            }
+        }
+
+    }
+}
