4a7215b0c5cc424429d9fe21e6f5e74bab068550
manishkumarmandane <bm0369@gmail.com>
2022-07-27 22:32:42 +0100

    modi

--- OddEvenumber.java
+++ OddEvenumber.java
@@ -5,7 +5,7 @@ public class OddEvenumber
     public static void main(String[] args)
     {
         int i ;
-        for(i=1; i<=20;i++)
+        for(i=0; i<=20;i++)
         {
             if (i % 2 == 0)
             {
